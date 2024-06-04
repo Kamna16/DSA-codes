704d8bb5da6d087e4e0cee82a2df7d12775b8562
kamna <kamnabhadoriya042@gmail.com>
2024-06-04 22:27:19 +0530

    sorting

--- sorting/problems/mergeIntervals.cpp
+++ sorting/problems/mergeIntervals.cpp
@@ -1,7 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
@@ -27,6 +26,24 @@ public:
     }
 };
 
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> ans;
+        sort(intervals.begin(),intervals.end());
+        for(int i=0;i<intervals.size();i++)
+        {
+            if(ans.empty() || intervals[i][0] > ans.back()[1])
+            {
+                ans.push_back(intervals[i]);
+            }
+            else{
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+            }
+        }
+        return ans;
+    }
+};
 
 int main(){
     
