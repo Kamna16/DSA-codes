4667ce304fe05e0121b9a7a899cef192dacc95c2
kamna <kamnabhadoriya042@gmail.com>
2024-06-29 01:33:26 +0530

    potd and recursion

--- Recursion/backtracking/NQueens.cpp
+++ Recursion/backtracking/NQueens.cpp
@@ -4,6 +4,53 @@
 #include<iostream>
 #include<vector>
 using namespace std;
+
+class Solution {
+public:
+    vector<vector<string>> ans;
+    bool isValid(int r, int c, vector<string> &board)
+    {
+        // check upwards
+        for(int i=r-1;i>=0;i--)
+        {
+            if(board[i][c] == 'Q') return false;
+        }
+        // check diagonals
+        for(int i=r-1, j=c-1;i>=0 && j>=0;i--,j--)
+        {
+            if(board[i][j] == 'Q') return false;
+        }
+        // check right
+        for(int i=r-1,j=c+1;i>=0 && j<board.size();i--,j++)
+        {
+            if(board[i][j] == 'Q') return false;
+        }
+        return true;
+    }
+    void solve(vector<string> &board,int row)
+    {
+        if(row >= board.size())
+        {
+            ans.push_back(board);
+            return;
+        }
+        for(int col=0;col<board.size();col++)
+        {
+            if(isValid(row,col,board))
+            {
+                board[row][col] = 'Q';
+                solve(board,row+1);
+                board[row][col] = '.';
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string> board(n,string(n,'.'));
+        solve(board, 0);
+        return ans;
+    }
+};
+
 class Solution {
 public:
     bool isSafe(int row, int col,vector<string> &board, int n )
