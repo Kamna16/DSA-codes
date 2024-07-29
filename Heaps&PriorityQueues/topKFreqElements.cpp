42f168725069ac8308369811ff726f8d950df66e
kamna <kamnabhadoriya042@gmail.com>
2024-07-29 18:07:54 +0530

    practice

--- Heaps&PriorityQueues/topKFreqElements.cpp
+++ Heaps&PriorityQueues/topKFreqElements.cpp
@@ -3,38 +3,28 @@ using namespace std;
 // https://leetcode.com/problems/top-k-frequent-elements/submissions/1068667552/
 class Solution {
 public:
-	vector<int> topKFrequent(vector<int>& nums, int k) {
-		vector<int> ans;
-		unordered_map<int,int> m ={};// for frequency
-		int n = nums.size();
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        int n = nums.size();
+        unordered_map<int,int> mp;
+        for(int i=0;i<n;i++)
+        {
+            mp[nums[i]]++;
+        }
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
 
-		// min heap
-		priority_queue<pair<int,int> , vector<pair<int,int>>, greater<pair<int,int>>> q;
+        for(auto x : mp)
+        {
+            pq.push({x.second,x.first});
+            while(pq.size() > k) pq.pop();
+        }
 
-		for(int i=0;i<n;i++)
-		{
-			m[nums[i]]++; // storing frequency of every elements
-		}
-		// traversing the map
-		for(auto i=m.begin(); i!=m.end(); i++)
-		{
-			q.push({i->second, i->first});
-			if(q.size()>k)
-			{
-				q.pop();
-			}
-		}
-		// push the top elements in ans vector
-		while(q.size()> 0)
-		{
-			ans.push_back(q.top().second);
-			q.pop();
-		}
-		return ans;
-	}
+        vector<int> ans;
+        while(!pq.empty())
+        {
+            int num = pq.top().second;
+            ans.push_back(num);
+            pq.pop();
+        }
+        return ans;
+    }
 };
-
-int main(){
-    
-    return 0;
-}
