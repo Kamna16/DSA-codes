41da26b4dceaf3a1853a444f924c83bf8f654a9f
kamna <kamnabhadoriya042@gmail.com>
2024-07-31 19:25:52 +0530

    practice

---
+++ Stack/onlinStockSpan.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class StockSpanner {
+public:
+    stack<pair<int,int>> st;
+    StockSpanner() {
+        
+    }
+    
+    int next(int price) {
+        int span = 1; 
+        while (!st.empty() && st.top().first <= price) {
+            span += st.top().second;
+            st.pop();
+        }
+        st.push({price, span});
+        return span;
+    }
+};
--- sliding window/maxofWindow.cpp
+++ sliding window/maxofWindow.cpp
@@ -1,5 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+// using deque
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& arr, int k) {
+        deque<int> dq;
+        vector<int> ans;
+
+        for (int i = 0; i < arr.size(); ++i) {
+            // Remove elements out of the current window
+            if (!dq.empty() && dq.front() == i - k) {
+                dq.pop_front();
+            }
+
+            // Remove elements smaller than the current element
+            while (!dq.empty() && arr[dq.back()] < arr[i]) {
+                dq.pop_back();
+            }
+
+            // Add current element at the back of the deque
+            dq.push_back(i);
+
+            // The front of the deque is the maximum element of the current window
+            if (i >= k - 1) {
+                ans.push_back(arr[dq.front()]);
+            }
+        }
+
+        return ans;
+    }
+};
+
 class Solution {
 public:
     vector<int> maxOfSubArray(vector<int>& arr, int k) {
